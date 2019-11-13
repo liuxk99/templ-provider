5fc6d311711791ca9138235971d9d23427895652
Thomas Liu <liuxk99@gmail.com>
2019-11-13 17:43:11 +0800

    <TEST>: testcase for ContentProvider

--- app/src/androidTestDebug/java/com/sj/providers/templ/TemplatesProviderTest.java
+++ app/src/androidTestDebug/java/com/sj/providers/templ/TemplatesProviderTest.java
@@ -1,10 +1,20 @@
 package com.sj.providers.templ;
 
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.test.ProviderTestCase2;
 
 import androidx.test.core.app.ApplicationProvider;
 
-class TemplatesProviderTest extends ProviderTestCase2 {
+import org.junit.Test;
+
+public class TemplatesProviderTest extends ProviderTestCase2 {
+    public TemplatesProviderTest() {
+        super(TemplatesProvider.class, "com.sj.providers.Template");
+    }
+
     public TemplatesProviderTest(Class providerClass, String providerAuthority) {
         super(providerClass, providerAuthority);
     }
@@ -15,4 +25,17 @@ class TemplatesProviderTest extends ProviderTestCase2 {
         setContext(ApplicationProvider.getApplicationContext());
     }
 
+    @Test
+    public void testcaseInsert() {
+        ContentProvider provider = getProvider();
+
+        ContentValues values = new ContentValues();
+        values.put(Templates.TITLE, "title-1");
+        values.put(Templates.ABSTRACT, "Content-1");
+        values.put(Templates.URL, "http://a.b.c");
+
+        Uri uri = provider.insert(Templates.CONTENT_URI, values);
+        String itemId = uri.getPathSegments().get(1);
+        System.out.println("id: " + itemId);
+    }
 }
